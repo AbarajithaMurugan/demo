c7ac233f77e9d8781aa78d906be946bd2c39fd70
AbarajithaMurugan <abarajitha1995murugan@gmail.com>
2020-12-17 14:34:15 +0530

    ovr

---
+++ src/test/java/org/gitclone/Coppy.java
@@ -0,0 +1,16 @@
+package org.gitclone;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Coppy {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Navin Vishal M\\Downloads\\Abara's\\eclipse\\configuration\\Demo\\driver\\chromedriver.exe");
+    WebDriver driver= new ChromeDriver();
+    driver.get("https://en-gb.facebook.com/login/");
+    driver.findElement(By.id("email")).sendKeys("abcbjdn");
+    driver.findElement(By.id("pass")).sendKeys("12345");
+    driver.findElement(By.id("loginbutton")).click();
+}
+}
